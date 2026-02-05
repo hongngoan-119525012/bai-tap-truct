e2a1fa5394bb48ccd95cc5535dea81336532e929
hongngoan <hongngoan2007@gmail.com>
2026-02-05 09:35:18 +0700

    update file

--- struct-matrix.c
+++ struct-matrix.c
@@ -1,2 +1,53 @@
+#include <stdio.h>
 #define SIZE 5
+typedef struct MA_TRAN
+{
+	int Matrix[SIZE][SIZE];
+	int c,d;
+}MT;
+
+void nhap(MT *a);
+void xuat(MT a);
+
+int main(void)
+{
+	MT a;
+	nhap(&a);
+	xuat(a);
+	
+	return 0;
+}
+
+void nhap(MT *a)
+{
+	printf("\nHay Nhap Dong Ma Tran: ");
+	scanf("%d", &a->d);
+
+	printf("\nHay Nhap Cot Ma Tran: ");
+	scanf("%d", &a->c);
+	
+	int i, j;
+	for(i=0; i<a->d; i++)
+	{
+		for(j=0; j<a->c; j++)
+		{
+			printf("\nNhap Gia Tri Dong %d, cot %d:  ", i+1, j+1);
+			scanf("%d",&a->Matrix[i][j]);
+		}
+	}
+}
+void xuat(MT a)
+{
+	int i,j;
+	for(i=0; i<a.d; i++)
+	{
+		for(j=0; j<a.c; j++)
+		{
+			printf("\nNhap Gia Tri Dong %d, cot %d: %d ", i+1, j+1, a.Matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
 
